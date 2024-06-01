f2f0add543bafbae45e65ae69fc8d7c7e380d079
Gaurang-0204 <salvigaurang0@gmail.com>
2024-05-31 23:40:18 -0700

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+
+import{ BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import LandingPage from './components/LandingPage/LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+     <Router>
+      <Routes>
+      <Route path="/" element={<LandingPage />} />
+      </Routes>
+     </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/LandingPage/LandingPage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './LandingPage.css'
+
+const LandingPage = () => {
+  return (
+    <div className="landing-p">
+      <div className="card">
+        <div className="left">
+         <h1>Fantsay-Lib</h1>
+         <p>Welcome to Fantsay-Lib, the ultimate e-library dedicated exclusively to the captivating world of fantasy literature. Here, magic comes to life through a vast collection of spellbinding tales, epic adventures, and mythical creatures, catering to every fantasy enthusiast's wildest imaginations. Whether you seek the heroics of valiant knights, the intrigue of ancient prophecies, or the wonder of magical realms, Fantsay-Lib offers a treasure trove of stories that transport you beyond the ordinary into the extraordinary. Dive into our enchanting library and embark on a journey through the limitless landscapes of fantasy, where every page promises a new adventure. </p>
+        </div>
+
+        <div className="right">
+          <p>Login </p>
+          <p>Signup</p>
+          <br></br>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
